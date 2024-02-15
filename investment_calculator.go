e4164fec3d081a2cc5b97338d07f99b2bd0ce08b
Seth Bolander <bol15015@byui.edu>
2024-02-15 15:00:04 -0700

    working with fmt.Scan()

--- investment_calculator.go
+++ investment_calculator.go
@@ -7,7 +7,16 @@ import (
 
 func main() {
 	const inflationRate = 2.5
-	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
+	var investmentAmount, years, expectedReturnRate float64
+
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
 	futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, years)
