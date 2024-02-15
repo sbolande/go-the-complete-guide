f4a2007c51cb771ed85fdb590fe81110481e0395
Seth Bolander <bol15015@byui.edu>
2024-02-15 14:37:04 -0700

    working with consts

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,8 +6,11 @@ import (
 )
 
 func main() {
+	const inflationRate = 2.5
 	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
 
 	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+	futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, years)
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
