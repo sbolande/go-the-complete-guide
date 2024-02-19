5e45dcb94f251ee546c790c66163386fb8827532
Seth Bolander <bol15015@byui.edu>
2024-02-19 11:56:49 -0700

    working with structs

---
+++ structs.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type user struct {
+	firstName string
+	lastName string
+	birthdate string
+	createdAt time.Time
+}
+
+func main() {
+	firstName := getUserData("Please enter your first name: ")
+	lastName := getUserData("Please enter your last name: ")
+	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	// ... do something awesome with that gathered data!
+	outputUserDetails()
+}
+
+func outputUserDetails() {
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
