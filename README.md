8f561d5bf4d484a764d3669abb287829e45dfd53
Seth Bolander <bol15015@byui.edu>
2024-02-19 11:00:59 -0700

    finished profit calculator

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ This is course work for the Udemy Course **Go: The Complete Guide** from Maximil
 
 - [Section 02: Go Essentials](https://github.com/sbolande/go-the-complete-guide/tree/section02)
   - Go modules
-  - Variables
+  - Variables and constants
   - Functions
+  - If/else and switch statements
   - String formatting
   - Reading and writing files
+  - Loops
--- profit_calculator.go
+++ profit_calculator.go
@@ -1,17 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+const financialsFileName = "finances.txt"
 
 func main() {
-	revenue := getUserInputFloat("Revenue")
-	expenses := getUserInputFloat("Expenses")
-	taxRate := getUserInputFloat("Tax Rate")
-
+	revenue, err1 := getUserInputFloat("Revenue")
+	expenses, err2 := getUserInputFloat("Expenses")
+	taxRate, err3 := getUserInputFloat("Tax Rate")
+	if err1 != nil || err2 != nil || err3 != nil { 
+		fmt.Println(err1, err2, err3)
+		return
+	}
+	
 	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
-	fmt.Printf("%.1f\n", ebt)
-	fmt.Printf("%.1f\n", profit)
-	fmt.Printf("%.1f\n", ratio)
+	writeFinancialsToFile(ebt, profit, ratio)
+}
+
+func getUserInputFloat(prompt string) (float64, error) {
+	var input float64
+	fmt.Print(prompt, ": ")
+	fmt.Scan(&input)
+	if input <= 0 {
+		return 0, errors.New("Value must be a positive number")
+	}
+	return input, nil
 }
 
 func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
@@ -21,9 +39,8 @@ func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, 
 	return ebt, profit, ratio
 }
 
-func getUserInputFloat(prompt string) float64 {
-	var input float64
-	fmt.Print(prompt, ": ")
-	fmt.Scan(&input)
-	return input
+func writeFinancialsToFile(ebt, profit, ratio float64) {
+	financialsText := fmt.Sprintf("EBT: %.1f | PROFIT: %.1f | RATIO: %.3f", ebt, profit, ratio)
+	fmt.Println(financialsText)
+	os.WriteFile(financialsFileName, []byte(financialsText), 0644)
 }
