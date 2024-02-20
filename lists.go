d1592ea784a4945ca9b02f7b61c16532fa82996c
Seth Bolander <bol15015@byui.edu>
2024-02-20 10:07:50 -0700

    working with arrays

---
+++ lists.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var productNames [4]string = [4]string{"A Book"}
+	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+	fmt.Println(productNames)
+	fmt.Println(prices)
+
+	fmt.Println(prices[0])
+}
